9cb75f071f37db49f07795026c2d6024194dae42
Vinayak Prataap <vprataap@Vinayaks-MacBook-Air.local>
2019-09-28 12:41:48 -0400

    Add ApiService

---
+++ apiService/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const Database = require('./db');
+const port = 9556;
+let app = express();
+require('dotenv').config();
+
+app.db = new Database(process.env.DB_HOST, process.env.DB_PORT);
+app.db.connect().then(() => {
+    app.get('/users', (req, res)=>{
+        app.db.getData({db: 'shusphere', table: 'users', field: 'id'}).then((data)=>{
+            res.json(data);
+        });
+    });
+    
+    app.get('/visitors', (req, res)=>{
+        app.db.getData({db: 'shusphere', table: 'visitors', field: 'id'}).then((data)=>{
+            res.json(data);
+        });
+    });
+    
+    app.get('/devices', (req, res)=>{
+        app.db.getData({db: 'shusphere', table: 'devices', field: 'id'}).then((data)=>{
+            res.json(data);
+        });
+    });
+    
+    app.get('/complaints', (req, res)=>{
+        app.db.getData({db: 'shusphere', table: 'complaints', field: 'id'}).then((data)=>{
+            res.json(data);
+        });
+    });
+    
+    app.get('/contacts', (req, res)=>{
+        app.db.getData({db: 'shusphere', table: 'contacts', field: 'id'}).then((data)=>{
+            res.json(data);
+        });
+    });
+    
+    app.listen(port, ()=>{
+        console.log("The App is listening on port: " + port);
+    });
+})
