b4a1a9e6268d15fa187d942574c8f8e880b43467
Vinayak Prataap <vprataap@Vinayaks-MacBook-Air.local>
2019-09-28 06:26:45 -0400

    Delete everything and start over :))))))))

--- scripts/requestMaker.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.get('https://script.google.com/macros/s/AKfycbxUqkLQLY3jfCm2GlTv3XGOW5SOTpvfO0H_hYhRr4hQtTNd56A/exec').then((res)=>{
-    console.log(res.data);
-});
